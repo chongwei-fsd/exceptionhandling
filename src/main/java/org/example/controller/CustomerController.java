f5641d9222067dae73105ac85f6c382419ec63d5
chongwei-fsd <ChongWei@genstudents.org>
2024-10-07 12:09:36 +0800

    Update CustomerController.java

--- src/main/java/org/example/controller/CustomerController.java
+++ src/main/java/org/example/controller/CustomerController.java
@@ -33,12 +33,12 @@ public class CustomerController {
     }
 
     @PostMapping
-    public ResponseEntity<Object> saveCustomer(@Valid @RequestBody Customer customer){
+    public ResponseEntity<Object> saveCustomer(@RequestBody @Valid Customer customer){
         return new ResponseEntity<>(customerService.save(customer), HttpStatus.OK);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Object> updateCustomer(@PathVariable("id") Long customerId, @RequestBody Customer customer){
+    public ResponseEntity<Object> updateCustomer(@PathVariable("id") Long customerId, @RequestBody @Valid Customer customer){
 
         Customer checkCustomer = customerService.findById(customerId).map(_customer ->{
             _customer.setName(customer.getName());
